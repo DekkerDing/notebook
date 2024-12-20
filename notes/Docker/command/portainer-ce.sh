a7ea1fd05cf5304accba8425f548dcf30817985e
Ding <Dekker160@163.com>
2024-12-20 18:19:01 +0800

    -- create -- [portainer-ce]

---
+++ notes/Docker/command/portainer-ce.sh
@@ -0,0 +1,12 @@
+docker run
+-itd \
+--restart=always \
+--privileged \
+--tty \
+--user 0 \
+--name="portainer" \
+-e TZ=Asia/Shanghai \
+-p 19000:9000 \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v /data/portainer:/data \
+6053537/portainer-ce
