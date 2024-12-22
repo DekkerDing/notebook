0d4f3fa8dec24524aa117608461a378a2fe05224
Ding <Dekker160@163.com>
2024-12-22 19:11:54 +0800

    -- update -- [docker-compose]

--- notes/basics.sh
+++ notes/basics.sh
@@ -57,6 +57,9 @@ journalctl -u docker -f
 
 systemctl enable docker
 
+curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 sudo apt-get install containerd.io
 
 
