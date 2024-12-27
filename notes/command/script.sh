d0ed4abea9c2b245fcf18f8e7cdef2a0daa0ca40
Ding <Dekker160@163.com>
2024-12-27 13:54:03 +0800

    -- init -- [redis-v7初始化]

---
+++ notes/command/script.sh
@@ -0,0 +1,15 @@
+docker run -itd \
+--name redis-node-1 \
+--net host \
+--privileged=true \
+-v /data/redis/share/redis-node-1:/data redis:6.0.8 \
+--cluster-enabled yes \
+--appendonly yes \
+--port 6381
+
+
+docker run -d --name redis-node-2 --net host --privileged=true -v /data/redis/share/redis-node-2:/data redis:6.0.8 --cluster-enabled yes --appendonly yes --port 6382
+docker run -d --name redis-node-3 --net host --privileged=true -v /data/redis/share/redis-node-3:/data redis:6.0.8 --cluster-enabled yes --appendonly yes --port 6383
+docker run -d --name redis-node-4 --net host --privileged=true -v /data/redis/share/redis-node-4:/data redis:6.0.8 --cluster-enabled yes --appendonly yes --port 6384
+docker run -d --name redis-node-5 --net host --privileged=true -v /data/redis/share/redis-node-5:/data redis:6.0.8 --cluster-enabled yes --appendonly yes --port 6385
+docker run -d --name redis-node-6 --net host --privileged=true -v /data/redis/share/redis-node-6:/data redis:6.0.8 --cluster-enabled yes --appendonly yes --port 6386
