12a74b17106d5aaa3fc3709de04f957be49288d1
Ding <Dekker160@163.com>
2025-02-21 12:14:56 +0800

    -- feat -- [Redis v6 内存淘汰]

--- examples/src/main/java/io/github/dekkerding/examples/config/RedisTemplateConfiguration.java
+++ examples/src/main/java/io/github/dekkerding/examples/config/RedisTemplateConfiguration.java
@@ -1,14 +1,25 @@
 package io.github.dekkerding.examples.config;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisTemplateConfiguration {
@@ -22,6 +33,7 @@ public class RedisTemplateConfiguration {
 
         ObjectMapper objectMapper =new ObjectMapper();
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        //objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
 
         //设置key和value的序列化规则
@@ -40,4 +52,30 @@ public class RedisTemplateConfiguration {
         return redisTemplate;
     }
 
+    public RedisCacheConfiguration redisCacheConfiguration(Long ttl){
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.registerModule(new JavaTimeModule());
+        //去掉各种@JsonSerialize注解的解析
+        objectMapper.configure(MapperFeature.USE_ANNOTATIONS,false);
+        //只针对非空的值进行序列化
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        //将类型序列化到属性json字符串中
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(ttl))
+        //.disableCachingNullValues()
+        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer));
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory factory){
+        RedisCacheConfiguration redisCacheConfiguration = redisCacheConfiguration(3600 * 24L);
+        return RedisCacheManager.builder(factory)
+                .cacheDefaults(redisCacheConfiguration)
+                .transactionAware().build();
+    }
 }
