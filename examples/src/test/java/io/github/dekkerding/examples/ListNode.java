3ca4d7c8e5b60baffd57092fb7644a23ee06ec3b
Ding <Dekker160@163.com>
2025-04-21 17:42:02 +0800

    -- fix -- [java:单项链表排序]

---
+++ examples/src/test/java/io/github/dekkerding/examples/ListNode.java
@@ -0,0 +1,79 @@
+package io.github.dekkerding.examples;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    @SpringBootTest
+    public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public static ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode mid = getMid(head);
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next = null;
+
+        left = sortList(left);
+        right = sortList(right);
+        return merge(left, right);
+    }
+
+    private static ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private static ListNode merge(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                tail.next = left;
+                left = left.next;
+            } else {
+                tail.next = right;
+                right = right.next;
+            }
+            tail = tail.next;
+        }
+        if (left != null) {
+            tail.next = left;
+        } else {
+            tail.next = right;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(3);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(1);
+        ListNode sortedList = sortList(listNode);
+        while (sortedList != null) {
+            System.out.println(sortedList.val);
+            sortedList = sortedList.next;
+        }
+    }
+}
