d4f198615c489baf0fd6ea2b9e5d12f07d7cb901
Ding <Dekker160@163.com>
2025-05-06 15:49:37 +0800

    -- fix -- [java:桶排序]

---
+++ examples/src/test/java/io/github/dekkerding/examples/bucket.java
@@ -0,0 +1,17 @@
+package io.github.dekkerding.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class bucket {
+
+    @Test
+    public void bucketSort(){
+
+    }
+
+    public void bucketSortCore(int[] arr){
+
+    }
+}
