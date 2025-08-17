c1d9297744db5bc379e3bc9fe301c3e5491756a3
Ding <Dekker160@163.com>
2025-08-17 21:47:36 +0800

    -- feat -- [缓存/redis-v7集成配置Redisson]

--- examples/src/main/java/io/github/dekkerding/examples/config/RedissonConfiguration.java
+++ examples/src/main/java/io/github/dekkerding/examples/config/RedissonConfiguration.java
@@ -3,13 +3,19 @@ package io.github.dekkerding.examples.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.integration.redis.util.RedisLockRegistry;
 
 import java.io.IOException;
 
 @Configuration
 public class RedissonConfiguration {
+
+    @Value("${spring.redis.key-prefix}")
+    private String keyPrefix;
 
     @Bean
     public RedissonClient redissonClient() throws IOException {
@@ -31,6 +37,17 @@ public class RedissonConfiguration {
                 .setPassword("drk@2025"); // 如果集群有密码认证
 
         return Redisson.create(config);
+    }
+
+    /**
+     * 注册Redis Lock注册器
+     *
+     * @param factory
+     * @return RedisLockRegistry
+     */
+    @Bean
+    public RedisLockRegistry redisLockRegistry(RedisConnectionFactory factory) {
+        return new RedisLockRegistry(factory, keyPrefix + "lock");
     }
 
 }
